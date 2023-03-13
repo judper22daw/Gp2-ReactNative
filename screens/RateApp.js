53f008905182f056ff7e5315879afda64170b594
judper22daw <judper22daw2@protonmail.com>
2023-03-13 19:51:39 +0100

    update RateApp

--- screens/RateApp.js
+++ screens/RateApp.js
@@ -16,8 +16,8 @@ const RateApp = () => {
         source={{ uri: 'https://via.placeholder.com/150' }}
         style={styles.image}
       />
-      <Text style={styles.title}>Títol de l'aplicació</Text>
-      <Text style={styles.description}>Descripció de l'aplicació.</Text>
+      <Text style={styles.title}>Share and help</Text>
+      <Text style={styles.description}>Aquesta app està dissenyada per a aquells que volen ajudar a combatre la pobresa al món i fer una diferència significativa en la vida de les persones necessitades.</Text>
       <View style={styles.rating}>
         <Text style={styles.ratingText}>Puntuació:</Text>
         <StarRating
