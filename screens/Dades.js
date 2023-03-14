5d38fcbf2b9aeb4cfda9b16322b265ac98f29dd7
judper22daw <judper22daw2@protonmail.com>
2023-03-14 19:27:08 +0100

    rename funcions

--- screens/Dades.js
+++ screens/Dades.js
@@ -5,22 +5,22 @@ import { LineChart } from 'react-native-charts-wrapper';
 
 const Dades = () => {
     // Per defecte, quan entrem a aquesta vista sempre es mostrara la opcio de Poblacio
-    const [selectedOption, setSelectedOption] = React.useState('Poblacio');
-    const [chartData, setChartData] = React.useState([]);
+    const [opcioSeleccionada, seleccio] = React.useState('Poblacio');
+    const [dadesTaula, setDadesTaula] = React.useState([]);
 
     const data = require('./dades.json');
-    const tableData = data[selectedOption];
+    const tableData = data[opcioSeleccionada];
 
-    const handleOptionChange = (value) => {
-        setSelectedOption(value);
+    const Opcio = (value) => {
+        seleccio(value);
     };
 
     const handleChartButtonClick = () => {
-        const chartData = Object.entries(tableData).map(([label, value]) => ({
+        const dadesTaula = Object.entries(tableData).map(([label, value]) => ({
             x: label,
             y: Number(value),
         }));
-        setChartData(chartData);
+        setDadesTaula(dadesTaula);
     };
 
     const renderItem = ({ item }) => (
@@ -34,8 +34,8 @@ const Dades = () => {
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', padding: 10, borderWidth: 1, borderColor: '#CCCCCC' }}>
             <Text style={{ fontSize: 24, fontWeight: 'bold', marginBottom: 20 }}>Dades de diferents països</Text>
             <Picker
-                selectedValue={selectedOption}
-                onValueChange={handleOptionChange}
+                selectedValue={opcioSeleccionada}
+                onValueChange={Opcio}
                 style={{ width: '100%' }}>
                 <Picker.Item label="Població" value="Poblacio" />
                 <Picker.Item label="PIB" value="Pib" />
@@ -46,10 +46,10 @@ const Dades = () => {
                 keyExtractor={(item) => item[0]}
                 style={{ borderTopWidth: 1, borderColor: '#CCCCCC' }}
             />
-            {chartData.length > 0 && (
+            {dadesTaula.length > 0 && (
                 <View style={{ height: 200 }}>
                     <LineChart
-                        data={{ dataSets: [{ label: selectedOption, values: chartData }] }}
+                        data={{ dataSets: [{ label: opcioSeleccionada, values: dadesTaula }] }}
                         xAxis={{ position: 'BOTTOM' }}
                         yAxis={{ position: 'LEFT' }}
                         style={{ flex: 1 }}
